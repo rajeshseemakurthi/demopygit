d10828e5dd79fade48e436ff53bb7204adf0e7c5
Sony <rajeshsingh.professional@gmail.com>
2021-09-25 20:56:42 +0530

    program completed within the project within full swing

--- demo.py
+++ demo.py
@@ -1,3 +1,7 @@
 print("Hello World")
 print("Welcome to Telusko")
-rundemo version
+a=int(input("Enter Your First Number"))
+b=int(input("Enter Your Second Number"))
+x=a+b
+for i in range(x):
+    print(i,end="")
